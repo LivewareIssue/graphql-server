96eb978317fe124e5613feff3a4947b2df57b311
Thomas <me@tom-bates.co.uk>
2024-09-25 00:35:22 +0100

    Fix viewer query

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -8,19 +8,13 @@ namespace Server.GraphQL;
 public class Query
 {
     [Authorize(Roles = ["Admin"])]
-    [GraphQLDescription("Find a user by their ID.")]
-    public async ValueTask<User> User(
-        [GraphQLDescription("The user's ID.")]
-        [ID]
-        string id,
-        UserManager<EntUser> userManager)
+    [GraphQLDescription("Lookup a user by their ID.")]
+    public async ValueTask<User> User([ID] string id, UserManager<EntUser> userManager)
         => await GraphQL.User.GetAsync(id, userManager);
 
     [AllowAnonymous]
     [GraphQLDescription("The currently authenticated user.")]
-    public async Task<User?> Viewer(
-        [Service] UserManager<EntUser> userManager,
-        [Service] ClaimsPrincipal claimsPrincipal)
+    public async Task<User?> Viewer(UserManager<EntUser> userManager, ClaimsPrincipal claimsPrincipal)
     {
         var user = await userManager.GetUserAsync(claimsPrincipal);
         if (user == null)
